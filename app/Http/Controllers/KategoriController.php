eaad357c8cf32b25faa3043617f636875e33a9de
Bayu Fanani <fanani707@gmail.com>
2019-05-26 13:35:57 +0700

    fix kategori detail

--- app/Http/Controllers/KategoriController.php
+++ app/Http/Controllers/KategoriController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Kategori;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
 
 class KategoriController extends Controller
@@ -20,10 +21,32 @@ class KategoriController extends Controller
     public function isi_kategori(Request $r)
     {
         $isi_kategori = Kategori::find($r->id_kategori);
+        $resep = $isi_kategori->resep;
+        $hasil = [];
+        $sekarang = Carbon::now();
+
+        foreach ($resep as $data) {
+            $waktu_post = $data->waktu_post->diffInDays($sekarang) > 2 ?
+                $data->waktu_post->format('d M Y H:i') : $data->waktu_post->diffForHumans($sekarang);
+            array_push($hasil, [
+                'id_resep' => $data->id_resep,
+                'judul' => $data->judul,
+                'foto' => $data->foto,
+                'bahan' => $data->bahan,
+                'langkah' => $data->langkah,
+                'waktu_post' => $waktu_post,
+                'kategori' => $data->kategori->kategori,
+                'username' => $data->username,
+                'nama' => $data->user->nama,
+                'like' => $data->like->count(),
+                'komentar' => $data->komentar->count()
+            ]);
+        }
+
         if (!is_null($isi_kategori)) {
             return response()->json([
                 'pesan' => 'sukses',
-                'data' => $isi_kategori->resep
+                'data' => $hasil
             ]);
         } else {
             return response()->json([
